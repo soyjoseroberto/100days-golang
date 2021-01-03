2173a6c529d366d7b59da725c37bf407a8382e4b
soyjoseroberto <jrodriguez18@gmail.com>
2021-01-03 11:48:24 -0500

    Returned greetings for multiple people.

--- hello/hello.go
+++ hello/hello.go
@@ -14,8 +14,11 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names.
+	names := []string{"Dahlia", "Lucia", "Arielle"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("Jose Roberto")
+	message, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console  and
 	// exit the program.
 	if err != nil {
